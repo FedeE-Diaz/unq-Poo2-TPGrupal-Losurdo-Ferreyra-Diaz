850e111a45813ef95e8db55a32fcd8aa22cbc5d6
Valen <valen.ferreyra@outlook.com>
2021-11-05 19:20:40 -0300

    se borran cosas de prueba

--- src/ar/edu/unq/po2/SistemaDeEstacionamientoMedido/SEM.java
+++ src/ar/edu/unq/po2/SistemaDeEstacionamientoMedido/SEM.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 
 public class SEM {
 		
-		private int hora;
 		private ArrayList<Usuario> listaDeUsuarios;
 		private ArrayList<Infraccion> infracciones;
 		private ArrayList<EstacionamientoVigente> estacionamientosActuales;
