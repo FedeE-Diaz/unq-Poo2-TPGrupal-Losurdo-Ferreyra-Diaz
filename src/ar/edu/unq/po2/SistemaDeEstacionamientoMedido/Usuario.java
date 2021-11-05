95f04c2732d9e3912e373f7ff171e8b8e1a9f130
unknown <valen.ferreyra@outlook.com>
2021-11-05 20:29:00 -0300

    se agregan clases Celular y GPS; refactoricé métodos en Usuario

--- src/ar/edu/unq/po2/SistemaDeEstacionamientoMedido/Usuario.java
+++ src/ar/edu/unq/po2/SistemaDeEstacionamientoMedido/Usuario.java
@@ -2,4 +2,20 @@ package ar.edu.unq.po2.SistemaDeEstacionamientoMedido;
 
 public class Usuario {
 
+	private int numTelefonoAsociado;
+	private int credito;
+	
+	public Usuario(int numTelefonoAsociado, int credito) {
+		super();
+		this.numTelefonoAsociado = numTelefonoAsociado;
+		this.credito = credito;
+	}
+	
+	public int getNumTelefonoAsociado() {
+		return numTelefonoAsociado;
+	}
+	public int getCredito() {
+		return credito;
+	}
+	
 }
