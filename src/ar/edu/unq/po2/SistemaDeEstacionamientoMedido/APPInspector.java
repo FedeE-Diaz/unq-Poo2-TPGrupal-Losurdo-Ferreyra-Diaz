5f64868188987519cd41008a95c637fd7d2bd444
unknown <valen.ferreyra@outlook.com>
2021-11-08 16:32:34 -0300

    se agrega clase APPInspector e Infraccion

---
+++ src/ar/edu/unq/po2/SistemaDeEstacionamientoMedido/APPInspector.java
@@ -0,0 +1,26 @@
+package ar.edu.unq.po2.SistemaDeEstacionamientoMedido;
+
+public class APPInspector {
+	
+	private SEM sem;
+	
+	public APPInspector(SEM sem, Celular celular) {
+		this.sem     = sem;
+	}
+	
+	public void verificarPatente(String patente) {
+		if (!this.esEstacionamientoVigente(patente)) {
+			this.altaDeInfraccion(patente);
+		}
+	}
+
+	private void altaDeInfraccion(String patente) {
+		sem.cargarInfraccion(patente);
+		
+	}
+
+	private boolean esEstacionamientoVigente(String patente) {
+		return sem.esEstacionamientoVigente(patente);
+	}
+
+}
--- src/ar/edu/unq/po2/SistemaDeEstacionamientoMedido/Infraccion.java
+++ src/ar/edu/unq/po2/SistemaDeEstacionamientoMedido/Infraccion.java
@@ -1,5 +1,19 @@
 package ar.edu.unq.po2.SistemaDeEstacionamientoMedido;
 
+import java.security.Timestamp;
+import java.util.Date;
+
 public class Infraccion {
+	
+	private Date fecha;
+	private Timestamp hora;
+	
+	public Infraccion(Date fecha, Timestamp hora) {
+		super();
+		this.fecha = fecha;
+		this.hora = hora;
+	}
+	
+	
 
 }
