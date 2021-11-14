b011fe66f7311ca470296adbbab88ff041eb56dc
unknown <valen.ferreyra@outlook.com>
2021-11-14 13:07:09 -0300

    Mas testeos sobre Gestion Registro

--- test/ar/edu/unq/po2/SistemaDeEstacionamientoMedido/GestionRegistroTest.java
+++ test/ar/edu/unq/po2/SistemaDeEstacionamientoMedido/GestionRegistroTest.java
@@ -2,6 +2,8 @@ package ar.edu.unq.po2.SistemaDeEstacionamientoMedido;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDateTime;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -20,11 +22,24 @@ class GestionRegistroTest {
 	}
 	
 	@Test
+	void testTodosLosRegistrosTienenSuNumeroDeControlDistinto() {
+		this.puntoDeVenta.iniciarEstacionamiento("ABC123", 5);
+		this.puntoDeVenta.cargarCredito(123123, 400);
+		this.puntoDeVenta.iniciarEstacionamiento("FCD43", 9);
+
+		assertEquals(sem.getMyRegistro().getRegistros().get(0).getNumeroDeControl(), 0);
+		assertEquals(sem.getMyRegistro().getRegistros().get(1).getNumeroDeControl(), 1);
+		assertEquals(sem.getMyRegistro().getRegistros().get(2).getNumeroDeControl(), 2);
+	}
+	
+	@Test
 	void testCuandoSeCompranHorasDeEstacionamiento_SeCreaUnRegistroConLosDatos() {
 		this.puntoDeVenta.iniciarEstacionamiento("ABC123", 5);
+		LocalDateTime fechaYHoraEsperados = LocalDateTime.now();
 		
 		assertEquals(sem.getMyRegistro().getCantidadDeRegistros(), 1);
 		assertEquals(sem.getMyRegistro().getRegistros().get(0).getPuntoDeVenta(), this.puntoDeVenta);
+		assertEquals(sem.getMyRegistro().getRegistros().get(0).getFechaYHora(), fechaYHoraEsperados, "Error en la fecha y hora");
 	}
 	
 	@Test
